61cc1d80c5e8c2059574e412f91b041b93bbc797
baixuejian <baixuejian0402@163.com>
2019-01-11 23:17:40 +0800

    update

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,10 +1,20 @@
-setInterval(task,3000);
-  function task(){
-    var ul=document.getElementsByClassName("carousel_inner")[0];
-    var img=ul.querySelector("li>a>img.show");
-    img.className="";
-    if(img===ul.children[4].children[0].children[0]){
-      ul.children[0].children[0].children[0].className="show";
-    }else
-    img.parentNode.parentNode.nextSibling.children[0].children[0].className="show";
-  }
+//循环播放一组图片
+//定义函数
+var ul=document.body.children[1].children[0];
+function task(){
+  var li=ul.querySelector("li.show");
+  li.className="";
+  if(li.nextElementSibling!=null){
+    li.nextElementSibling.className="show";
+  }else 
+  ul.children[0].className="show"; 
+}
+//启动定时器
+var n=setInterval(task,3000);
+//清除定时器
+ul.onmouseover=function(){
+  clearInterval(n);
+}
+ul.onmouseout=function(){
+  n=setInterval(task,3000);
+}
