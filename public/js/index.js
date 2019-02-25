a86ab64ee3bd7979f53036f0174b2bd06245f13a
baixuejian <baixuejian0402@163.com>
2019-02-25 23:09:34 +0800

    update details.js

--- public/js/index.js
+++ public/js/index.js
@@ -14,60 +14,62 @@ $("a").on("click",function(e){
   e.preventDefault();
 })
 
-// 滚动楼层
-var floor=$(".container>.floor");
-var winH=$(window).height();
-window.onscroll=function(){
-  //获得滚动过的距离——网页顶部超出文档显示区顶部的距离
-  var scrollTop=document.body.scrollTop||document.documentElement.scrollTop;
-  if(scrollTop>=766 && scrollTop<1482){
-    $(".layout").addClass("show")
-    .children(":first").addClass("scroll")
-    .siblings().removeClass("scroll");
-  }else if(scrollTop>=1482 && scrollTop<2198){
-    $(".layout").addClass("show")
-    .children(":eq(1)").addClass("scroll")
-    .siblings().removeClass("scroll");
-  }else if(scrollTop>=2198 && scrollTop<2914){
-    $(".layout").addClass("show")
-    .children(":eq(2)").addClass("scroll")
-    .siblings().removeClass("scroll");
-  }else if(scrollTop>=2914 && scrollTop<3630){
-    $(".layout").addClass("show")
-    .children(":last").addClass("scroll")
-    .siblings().removeClass("scroll");
-  }
-  else $(".layout").removeClass("show").children().css("background","");
-}
-$(".layout").on("click","li",function(){
-  var t=floor.eq($(this).index()).offset().top-winH/2;
-  $("body,html").animate({
-    "scrollTop":t
-  },500);
-})
-
+// // 滚动楼层
 // var floor=$(".container>.floor");
 // var winH=$(window).height();
-// var li=$(".layout").find("li");
-// var scrollTop=document.body.scrollTop||document.documentElement.scrollTop;
-// $(window).scroll(function(){
-//   floor.each(function(){
-//     if(scrollTop>$(this).offset().top-winH/2){
-//       $(".layout").addClass("show");
-//       li.removeClass("scroll");
-//       li.eq("$(this).index()").addClass("scroll");
-//     }
-//     if(scrollTop<floor.eq("0").offset().top-winH/2 && scrollTop>floor.eq("3").offset().top-winH/2){
-//       $(".layout").removeClass("show");
-//     }
-//   })
-// })
+// window.onscroll=function(){
+//   //获得滚动过的距离——网页顶部超出文档显示区顶部的距离
+//   var scrollTop=document.body.scrollTop||document.documentElement.scrollTop;
+//   if(scrollTop>=766 && scrollTop<1482){
+//     $(".layout").addClass("show")
+//     .children(":first").addClass("scroll")
+//     .siblings().removeClass("scroll");
+//   }else if(scrollTop>=1482 && scrollTop<2198){
+//     $(".layout").addClass("show")
+//     .children(":eq(1)").addClass("scroll")
+//     .siblings().removeClass("scroll");
+//   }else if(scrollTop>=2198 && scrollTop<2914){
+//     $(".layout").addClass("show")
+//     .children(":eq(2)").addClass("scroll")
+//     .siblings().removeClass("scroll");
+//   }else if(scrollTop>=2914 && scrollTop<3630){
+//     $(".layout").addClass("show")
+//     .children(":last").addClass("scroll")
+//     .siblings().removeClass("scroll");
+//   }
+//   else $(".layout").removeClass("show").children().css("background","");
+// }
 // $(".layout").on("click","li",function(){
 //   var t=floor.eq($(this).index()).offset().top-winH/2;
 //   $("body,html").animate({
 //     "scrollTop":t
 //   },500);
 // })
+
+var floor=$(".container>.floor");
+var winH=$(window).height();
+var li=$(".layout").find("li");
+$(window).scroll(function(){
+  var scrollTop=document.body.scrollTop||document.documentElement.scrollTop;
+  console.log(scrollTop);
+  floor.each(function(){
+    if(scrollTop>=floor.first().offset().top-winH/2 &&scrollTop<=floor.last().offset().top-winH/2){
+      if(scrollTop>=$(this).offset().top-winH/2){
+        $(".layout").addClass("show");
+        li.removeClass("scroll");
+        li.eq($(this).index()-1).addClass("scroll");
+      }
+    }else{
+      $(".layout").removeClass("show");
+    }
+  })
+})
+$(".layout").on("click","li",function(){
+  var t=floor.eq($(this).index()).offset().top-winH/2;
+  $("body,html").animate({
+    "scrollTop":t
+  },500);
+})
 
 // 轮播图
 //循环播放一组图片
