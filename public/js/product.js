eb024c0191df33787a103ab83b03f0be865d1cdb
baixuejian <baixuejian0402@tom.com>
2019-03-04 19:37:38 +0800

    update

--- public/js/product.js
+++ public/js/product.js
@@ -7,4 +7,32 @@ $(function(){
     .mouseout(function(){
         $(".guid-shopping>h3").next().hide();
     })
+    //轮播图
+    //创建定时器，循环播放一组图片
+    var i=0,timer;
+    function loop(i,next){
+        timer=setInterval(()=>{
+            if(i>=2){next=0;i=-1;}
+            $(".carousel-inner>div").eq(next).addClass("active").siblings().removeClass("active");
+            $(".carousel-indicators>li").eq(next).addClass("current").siblings().removeClass("current");
+            i=next;
+            next++;
+        },3000);
+    }
+    loop(i,i+1);
+    //给指示器绑定鼠标移入，移出事件
+    $(".carousel-indicators>li").hover(
+        function(){
+            var i=$(this).index();
+            clearInterval(timer);
+            $(this).addClass("current")
+            .siblings().removeClass("current");
+            $(".carousel-inner>div").eq(i).addClass("active")
+            .siblings().removeClass("active");
+        },
+        function(){
+            setInterval(loop(i,i+1),3000);
+        }
+    )
+    //给左右箭头绑定点击事件
 })
--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const pool=requeir("./pool");
 var server=express();
 server.listen(3000);
 server.use(express.static('public'));
