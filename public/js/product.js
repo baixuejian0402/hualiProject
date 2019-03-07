36c209eaed708eacfd8b83072d87880139f62b10
baixuejian <baixuejian0402@tom.com>
2019-03-07 20:44:47 +0800

    update

--- public/js/product.js
+++ public/js/product.js
@@ -1,14 +1,14 @@
 $(function(){
     $(".dropdown-menu").hide();
-    //导航栏下拉
+    //1 导航栏下拉
     $(".guid-shopping>h3").parent().mouseover(function(){
         $(".guid-shopping>h3").next().show();
     })
     .mouseout(function(){
         $(".guid-shopping>h3").next().hide();
     })
-    //轮播图
-    //创建定时器，循环播放一组图片
+    //2 轮播图
+    //2.1创建定时器，循环播放一组图片
     var i=0,timer;
     var length=$(".carousel-inner>li").length-1;
     function loop(i,next){
@@ -21,7 +21,7 @@ $(function(){
         },3000);
     }
     loop(i,i+1);
-    //给指示器绑定鼠标移入，移出事件
+    //2.2 给指示器绑定鼠标移入，移出事件
     $(".carousel-indicators>li").hover(
         function(){
             var i=$(this).index();
@@ -33,7 +33,7 @@ $(function(){
             setInterval(loop(i,i+1),3000);
         }
     )
-    //给左右箭头绑定点击事件
+    //2.3给左右箭头绑定点击事件
     $(".carousel>a").click(function(e){
         e.preventDefault();
         //清除定时器
@@ -51,11 +51,18 @@ $(function(){
         //3s后恢复定时器
         setInterval(loop(i,i+1),3000);
     })
-    //封装一个函数控制指示符跟对应的图片显示
+    //2.4 封装一个函数控制指示符跟对应的图片显示
     function task(i){
         $(".carousel-indicators>li").eq(i).addClass("current")
         .siblings().removeClass("current");
         $(".carousel-inner>li").eq(i).addClass("active")
         .siblings().removeClass("active");
     }
+    //3 右侧快速导航栏
+    $(".quick").on("mouseenter","ul>li>a",function(){
+        $(this).next().show();
+    })
+    $(".quick").on("mouseleave","ul>li>a",function(){
+        $(this).next().hide();
+    })
 })
